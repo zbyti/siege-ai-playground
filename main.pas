062a6ccad78287d5d7c9adbb31c22d1531fc315f
zbyti <zbyti@yahoo.com>
2021-02-18 20:42:26 +0100

    refactor

--- main.pas
+++ main.pas
@@ -10,32 +10,13 @@
 
 //-----------------------------------------------------------------------------
 
-procedure human; // brain = 0
-begin
-  checkJoyStatus; newDir := ply.dir;
-  case joyStatus of
-    JOY_UP    : if ply.dir <> JOY_DOWN  then newDir := JOY_UP;
-    JOY_DOWN  : if ply.dir <> JOY_UP    then newDir := JOY_DOWN;
-    JOY_LEFT  : if ply.dir <> JOY_RIGHT then newDir := JOY_LEFT;
-    JOY_RIGHT : if ply.dir <> JOY_LEFT  then newDir := JOY_RIGHT;
-  end;
-
-  if (newDir and availDir) = 0 then begin
-    ply.isAlive := false; ply.head := PLY_CRASH; Dec(alive);
-  end;
-end;
-
-//-----------------------------------------------------------------------------
-
 procedure playerMove;
 begin
   if ply.isAlive then begin
-    checkAvailDir;
 
-    if availDir = 0 then begin
-      ply.isAlive := false; Dec(alive);
-      putChar(ply.x, ply.y, PLY_BUSTED, ply.colour + $80);
-    end else begin
+    checkAvailDir;
+    if availDir = 0 then drawBustedPlayer
+    else begin
 
       case ply.brain of
         PLY_CTRL    : human;
@@ -45,29 +26,10 @@ begin
         AI_MIRROR   : aiMirror;
       end;
 
-      drawTail; ply.dir := newDir;
-
-      case newDir of
-        JOY_UP    : Dec(ply.y);
-        JOY_DOWN  : Inc(ply.y);
-        JOY_LEFT  : Dec(ply.x);
-        JOY_RIGHT : Inc(ply.x);
-      end;
-
-      putChar(ply.x, ply.y, ply.head, ply.colour);
+      drawPlayer;
     end;
 
   end;
-
-end;
-
-//-----------------------------------------------------------------------------
-
-procedure startScreen;
-begin
-  putChar(player1.x, player1.y, player1.head, player1.colour + $80);
-  repeat checkJoyStatus until joyStatus = JOY_FIRE;
-  putChar(player1.x, player1.y, player1.head, player1.colour);
 end;
 
 //-----------------------------------------------------------------------------
@@ -97,8 +59,9 @@ begin
     initScore; gameOver := false; level := 1;
 
     repeat mainLoop until isGameOver;
-    showScore;
 
-    pause(200);
+    showScore; pause(200);
   until false;
 end.
+
+//-----------------------------------------------------------------------------
